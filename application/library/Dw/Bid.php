5c74d381358c2e4361639fea1a5945d198d91486
p2pbuy <p2pbuy@p2pbuy.net>
2014-06-12 12:56:12 +0800

    开发
    竞价流程

--- application/library/Dw/Bid.php
+++ application/library/Dw/Bid.php
@@ -19,6 +19,20 @@ class Dw_Bid extends Dw_Abstract{
 		return $result;
 	}
 	
+	//通过接口竞价
+	public static function bidBuyOrderByApi($info){
+		try{
+			$aclConf = Tools_Conf::get('Api_ACL');
+			$info['source'] = 'web';
+			$info['sign'] = md5($aclConf[$info['source']]['name'].$info['boid'].$info['bidprice'].$info['uid'].$aclConf[$info['source']]['secret_key']);
+			$re = Api_Bid::bidBuyOrder($info);
+			$result = json_decode($re,true);
+		}catch(Exception $e){
+			return false;
+		}
+		return $result;
+	}
+	
 	//将竞价写入数据库
 	public static function bidBuyOrderByDb($info){
 		try{
