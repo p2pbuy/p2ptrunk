1358905f97d706a358aa2223750088d772c179dc
p2pbuy <p2pbuy@p2pbuy.net>
2014-12-09 15:52:12 +0800

    开发
    我的订单

--- application/controllers/Order/Getiframe.php
+++ application/controllers/Order/Getiframe.php
@@ -21,46 +21,55 @@ class Order_GetiframeController extends AbstractController{
 		foreach( $headers as $n => $v ) {
 			$headerArr[] = $n .':' . $v;
 		}
-		$headerArr[] = 'Accept: */*';
+		$headerArr[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
+		$headerArr[] = 'Accept-Encoding: gzip, deflate';
+		$headerArr[] = 'Accept-Language: zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3';
 		$headerArr[] = 'User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36';
 		$headerArr[] = 'Connection: Keep-Alive';
-		$user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36';
+		$headerArr[] = 'Host: redirect.viglink.com';
+		$headerArr[] = 'User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36';
+		//$user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36';
 		
 		if(!empty($thirdUrl)){
 			$ch = curl_init($thirdUrl);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_HTTPHEADER , $headerArr );
-			curl_setopt($ch, CURLOPT_USERAGENT,$user_agent);
+			//curl_setopt($ch, CURLOPT_HTTPHEADER , $headerArr );
+			//curl_setopt($ch, CURLOPT_USERAGENT,$user_agent);
 			curl_setopt($ch, CURLOPT_HEADER, 0);
 			$content = curl_exec($ch) ;
 			$curlInfo = curl_getinfo($ch);
 			curl_close($ch);
 		}
-		if($curlInfo['http_code'] == 302){
-			$ch = curl_init($curlInfo['redirect_url']);
+		
+		//兼容302 301跳转 重复5次就结束
+		$i = 0;
+		while($curlInfo['http_code'] == 302 || $curlInfo['http_code'] == 301){
+			$url = $curlInfo['redirect_url'];
+			$i++;
+			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_HTTPHEADER , $headerArr );
-			curl_setopt($ch, CURLOPT_USERAGENT,$user_agent);
+			//curl_setopt($ch, CURLOPT_HTTPHEADER , $headerArr );
+			//curl_setopt($ch, CURLOPT_USERAGENT,$user_agent);
 			curl_setopt($ch, CURLOPT_HEADER, false);
 			$content = curl_exec($ch) ;
 			$curlInfo = curl_getinfo($ch);
 			curl_close($ch);
+			if($i > 5){
+				break;
+			}
 		}
-		if($curlInfo['http_code'] == 301){
-			$ch = curl_init($curlInfo['redirect_url']);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_HTTPHEADER , $headerArr );
-			curl_setopt($ch, CURLOPT_USERAGENT,$user_agent);
-			curl_setopt($ch, CURLOPT_HEADER, false);
-			$content = curl_exec($ch) ;
-			$curlInfo = curl_getinfo($ch);
-			curl_close($ch);
-		}
-		if($curlInfo['http_code'] == 400){
+
+		if($curlInfo['http_code'] == 400 || $curlInfo['http_code'] != 200){
 			$content = '暂时无法显示';
 		}
 		
-		$content = preg_replace("/<script[^>]*?>.*?<\/script>/smi", "", $content);
+		//获得url域名
+		$urlArr = parse_url($url);
+		
+		if(in_array($urlArr['host'],Tools_Conf::get('Domain.domain.noflitejs'))){
+			$content = preg_replace("/<script[^>]*?>.*?<\/script>/smi", "", $content);
+		}
+		
 		
 		echo $content;
 		return true;
--- application/controllers/Order/Myorder.php
+++ application/controllers/Order/Myorder.php
@@ -8,51 +8,65 @@ class Order_MyorderController extends AbstractController{
 	public $authorize = self::MUSTLOGIN;
 	public $tpl = 'order/myorder.phtml';
 	public function hookAction(){
+		$showType = (Comm_Context::get('showtype','str')) ? Comm_Context::get('showtype','str') : 'myorder';
+		
 		$info['uid'] = $this->uid;
 		$myBuyOrderids = '';
 		$myBidOrders = array();
 		
 		//获得我发布的订单列表
-		$myBuyOrders = Dr_Order::showBuyOrderByUidByApi($info);
-		$myTakeOrders = Dr_Order::showTakeOrderByUidByApi($info);
-		//我的出价信息
-		$myBidInfos = Dr_Bid::getBidInfoByUidByApi($info);
-		unset($info);
-		//我曾经出过价的订单
-		foreach($myBidInfos as $myBidInfo){
-			$boids .= $myBidInfo['boid'].',';
-		}
-		$boids = substr($boids,0,-1);
-		$info['boids'] = $boids;
-		$myBidOrders = Dr_Order::showBuyOrderByBoidsByApi($info);
-
-		unset($info);
-		$myBuyOrders = ($myBuyOrders) ? $myBuyOrders : array();
-		
-		foreach($myBuyOrders as $myBuyOrder){
-			$myBuyOrderids .= $myBuyOrder['boid'].',';
-		}
-		$myBuyOrderids = substr($myBuyOrderids, 0, -1);
-		
-		$info['boids'] = $myBuyOrderids;
-		$bidPricesTmp = Dr_Bid::getBidPriceByBoidsByApi($info);
-		
-		if($bidPricesTmp){
-			//按照同一uid排重出价
-			foreach($bidPricesTmp as $boid => $bidPriceTmp){
-				foreach($bidPriceTmp as $bidPrice){
-					$bidPriceByUid[$bidPrice['uid']] = $bidPrice;
+		switch($showType){
+			case '':
+			case 'myorder':
+				//我的订单
+				$myBuyOrders = Dr_Order::showBuyOrderByUidByApi($info);
+				break;
+			case 'tokenorder':
+				//我出过价的订单
+				$myTakeOrders = Dr_Order::showTakeOrderByUidByApi($info);
+				break;
+			case 'bidorder':
+				//我的出价信息
+				$myBidInfos = Dr_Bid::getBidInfoByUidByApi($info);
+				unset($info);
+				//我曾经出过价的订单
+				foreach($myBidInfos as $myBidInfo){
+					$boids .= $myBidInfo['boid'].',';
 				}
-				$bidPrices[$boid] = $bidPriceByUid;
-			}
+				$boids = substr($boids,0,-1);
+				$info['boids'] = $boids;
+				$myBidOrders = Dr_Order::showBuyOrderByBoidsByApi($info);
+		
+				unset($info);
+				$myBuyOrders = ($myBuyOrders) ? $myBuyOrders : array();
+				
+				foreach($myBuyOrders as $myBuyOrder){
+					$myBuyOrderids .= $myBuyOrder['boid'].',';
+				}
+				$myBuyOrderids = substr($myBuyOrderids, 0, -1);
+				
+				$info['boids'] = $myBuyOrderids;
+				$bidPricesTmp = Dr_Bid::getBidPriceByBoidsByApi($info);
+				
+				if($bidPricesTmp){
+					//按照同一uid排重出价
+					foreach($bidPricesTmp as $boid => $bidPriceTmp){
+						foreach($bidPriceTmp as $bidPrice){
+							$bidPriceByUid[$bidPrice['uid']] = $bidPrice;
+						}
+						$bidPrices[$boid] = $bidPriceByUid;
+					}
+				}
+				break;
+			default:
+				break;
 		}
 		
 		//如果是管理员，展示所有订单
 		if($this->viewer['uid'] == Tools_Conf::get('Manager.uid')){
-			$allMyOrders = Dr_order::showBuyOrderByApi();
+			$allMyOrders = Dr_Order::showBuyOrderByApi();
 			$data['isShowAllBuyOrder'] = true;
 		}
-		
 		
 		$data['viewer'] = $this->viewer;
 		$data['myBuyOrders'] = $myBuyOrders;
@@ -60,6 +74,7 @@ class Order_MyorderController extends AbstractController{
 		$data['myBidOrders'] = ($myBidOrders) ? $myBidOrders : array();
 		$data['allBuyOrders'] = ($allMyOrders) ? $allMyOrders : array();
 		$data['bidPrices'] = $bidPrices;
+		$data['showType'] = $showType;
 		$data['nick'] = $this->viewer['nick'];
 		$data['usertype'] = ($this->viewer['extends']['type'] == 2) ? '我是买手' : '我是买家';
 		$data['islogined'] = (empty($this->viewer)) ? false : true;
